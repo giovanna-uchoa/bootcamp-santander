57388bf192f9e0614f37950c750ee2905896b6ba
giovannauchoagomes <giovannauchoa@usp.br>
2024-05-16 17:48:58 -0300

    Atualizando arquivos da interface NavegadorInternet

--- dio-java-basico/desafio-poo/src/NavegadorInternet.java
+++ dio-java-basico/desafio-poo/src/NavegadorInternet.java
@@ -1,4 +1,4 @@
-public interface NavegadorNaInternet {
+public interface NavegadorInternet {
     public void exibirPagina(String url);
     public void adicionarAba();
     public void atualizarPagina();
